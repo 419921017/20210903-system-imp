5ea56c022250ff718e0b08a00d8e374602cc70e2
power_840 <liuyu19921017@gmail.com>
2021-09-06 17:21:18 +0800

    pj init

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env) => {
+  const config = {
+    mode: 'development',
+    output: {
+      filename: 'index.js',
+      path: path.resolve(__dirname, 'dist'),
+      libraryTarget: env.production ? 'system' : '',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: { loader: 'babel-loader' },
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    plugins: [
+      !env.production &&
+        new HtmlWebpackPlugin({
+          template: path.resolve(__dirname, 'public', 'index.html'),
+        }),
+    ].filter(Boolean),
+    externals: env.production ? ['react', 'react-dom'] : [],
+  };
+
+  return config;
+};
